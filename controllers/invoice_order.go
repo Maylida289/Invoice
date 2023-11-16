f7649d25b5f995e081017ccb73df11f1ea005e4e
Maylida289 <4519210082@univpancasila.ac.id>
2023-11-16 17:15:39 +0700

    edit controller

--- controllers/invoice_order.go
+++ controllers/invoice_order.go
@@ -1,42 +1,35 @@
 package controllers
 
 import (
-	database "Invoice/db"
+	"Invoice/db"
 	"Invoice/models"
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
 type InvoiceReq struct { //untuk memparsing data yang diminta client sehingga dapat diproses ke sarvernya
-	ID_invoice uint   `json:"id" param:"id"`
-	Subject    string `json:"suject" validate:"required"`
-	Start_date string `json:"start_date" validate:"required"`
-	Due_date   string `json:"due_date" validate:"required"`
+	ID_customer      int     `json:"id_customer" param:"id"`
+	Subject          string  `json:"subject" validate:"required"`
+	Start_date       string  `json:"start_date" validate:"required"`
+	Due_date         string  `json:"due_date" validate:"required"`
+	Customer_name    string  `json:"customer_name" validate:"required"`
+	Customer_address string  `json:"customer_address" validate:"required"`
+	Item_name        string  `json:"item_name" validate:"required"`
+	Quantity         float64 `json:"quantity" validate:"required"`
+	Total_price      float64 `json:"total_price" validate:"required"`
 }
-type CustomerReq struct { //untuk memparsing data yang diminta client sehingga dapat diproses ke sarvernya
-	ID_customer      uint   `json:"id" param:"id"`
-	Customer_name    string `json:"customer_name" validate:"required"`
-	Customer_address string `json:"customer_address" validate:"required"`
-}
-type itemsReq struct { //untuk memparsing data yang diminta client sehingga dapat diproses ke sarvernya
-	ID_item     uint    `json:"id" param:"id"`
-	Item_name   float64 `json:"item_name" validate:"required"`
-	Quantity    float64 `json:"quantity" validate:"required"`
-	Unit_Price  float64 `json:"unit_price" validate:"required"`
-	Total_price float64 `json:"total_price" validate:"required"`
-	Grand_total float64 `json:"grand_total" validate:"required"`
+type BayarReq struct {
+	ID_bayar   int     `json:"id_bayar" param:"id"`
+	TotalAmout float64 `json:"total" validate:"required"`
 }
 
-var InvoiceList = make(map[int]models.Invoice_detail, 0)
 var CustomerList = make(map[int]models.Customer, 0)
-var ItemList = make(map[int]models.Item, 0)
 
 var LastInvoice int = 1
-var LastCustomer int = 1
-var LastItem int = 1
 
-func CreateInvoice(c echo.Context) (err error) { //menambahkan data konsumen
+func CreateCustomer(c echo.Context) (err error) { //menambahkan data konsumen
 	req := new(InvoiceReq)
 	if err = c.Bind(req); err != nil {
 		return c.JSON(http.StatusBadRequest, nil)
@@ -47,50 +40,61 @@ func CreateInvoice(c echo.Context) (err error) { //menambahkan data konsumen
 	}
 
 	//static API
-	invoice := models.Invoice_detail{
-		ID_invoice: uint(LastInvoice),
-		Subject:    req.Subject,
-		Start_date: req.Start_date,
-		Due_date:   req.Due_date,
+	// admin := models.Total_price{
+	// 	ID:        LastTotal,
+	// 	Price:     req.Price,
+	// 	Exchange:  req.Exchange,
+	// 	Pair:      req.Pair,
+	// 	PairPrice: req.PairPrice,
+	// 	Volume:    req.Volume,
+	// }
+	// TotalList[LastTotal] = admin
+	// LastTotal++
+
+	db := db.DBManager()
+	total := models.Customer{
+		// ID:        LastTotal,
+		Subject:          req.Subject,
+		Start_date:       req.Start_date,
+		Due_date:         req.Due_date,
+		Customer_name:    req.Customer_name,
+		Customer_address: req.Customer_address,
+		Item_name:        req.Item_name,
+		Quantity:         req.Quantity,
+		Total_price:      req.Total_price,
 	}
-	InvoiceList[LastInvoice] = invoice
+
+	CustomerList[LastInvoice] = total
 	LastInvoice++
 
-	db := database.DBManager()
-	// if err := db.Create(&admin).Error; err != nil {
-	// 	// Handle kesalahan yang terjadi saat menyimpan ke database
-	// 	return c.JSON(http.StatusInternalServerError, err.Error())
-	// }
-	result := db.Create(&invoice)
+	fmt.Println("user:", total)
+	result := db.Create(&total)
 
 	return c.JSON(http.StatusCreated, result)
 }
 
-func CreateCustomer(c echo.Context) (err error) { //menambahkan data konsumen
-	req := new(CustomerReq)
-	if err = c.Bind(req); err != nil {
-		return c.JSON(http.StatusBadRequest, nil)
-	}
-
-	if err = c.Validate(req); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-
+func GetInvoice(c echo.Context) (err error) { //menampilkan seluruh data konsumen
 	//static API
-	customer := models.Customer{
-		ID_customer:      uint(LastCustomer),
-		Customer_name:    req.Customer_name,
-		Customer_address: req.Customer_address,
-	}
-	CustomerList[LastCustomer] = customer
-	LastCustomer++
+	db := db.DBManager()
 
-	db := database.DBManager()
-	if err := db.Create(&customer).Error; err != nil {
-		// Handle kesalahan yang terjadi saat menyimpan ke database
+	var invoices []models.Customer
+	if err := db.Find(&invoices).Error; err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
-	result := db.Create(&customer)
 
-	return c.JSON(http.StatusCreated, result)
+	var responseInvoices []models.ResponseInvoice
+	for _, invoice := range invoices {
+		responseInvoice := models.ResponseInvoice{
+			Subject:          invoice.Subject,
+			Start_date:       invoice.Start_date,
+			Due_date:         invoice.Due_date,
+			Customer_name:    invoice.Customer_name,
+			Customer_address: invoice.Customer_address,
+			Total:            invoice.Total_price,
+		}
+
+		responseInvoices = append(responseInvoices, responseInvoice)
+	}
+
+	return c.JSON(http.StatusOK, responseInvoices)
 }
