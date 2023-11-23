6fc81756babe4da6d2d7a213bc2fb8876e690e2c
Maylida289 <4519210082@univpancasila.ac.id>
2023-11-24 01:26:21 +0700

    edit validator

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import (
 	"Invoice/db"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
 )
+
+type CustomValidator struct {
+	Validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.Validator.Struct(i)
+}
 
 func main() {
 	db.Init()
 	e := echo.New()
+	e.Validator = &CustomValidator{
+		Validator: validator.New(),
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		result := map[string]string{
 			"response": "200",
@@ -18,12 +31,15 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, result)
 	})
-	e.POST("/add/invoice", controllers.CreateInvoice)
+	e.POST("/add/customer", controllers.CreateCustomer)
 
-	// Rute untuk membuat rincian faktur
-	e.GET("/invoice_details", controllers.GetInvoice)
+	//memasukan harga
+	e.POST("/input/harga", controllers.CreateHarga)
+
+	//memasukan harga dari masing-masing quantity
+	e.POST("/input/quantity", controllers.CreateQuantity)
 
 	// Rute untuk membuat item faktur
 	// e.POST("/items", controllers.CreateItem)
-	e.Logger.Fatal(e.Start(":8070"))
+	e.Logger.Fatal(e.Start("localhost:8076"))
 }
